8777b2422207ab9a21fefb57af1468e7d9bf4243
Andrew Tanny Liem <andrew.t.liem@gmail.com>
2025-07-14 19:51:31 +0800

    Initial Commit

---
+++ MACPDFCompressorTests/MACPDFCompressorTests.swift
@@ -0,0 +1,17 @@
+//
+//  MACPDFCompressorTests.swift
+//  MACPDFCompressorTests
+//
+//  Created by Andrew Tanny Liem on 14/07/25.
+//
+
+import Testing
+@testable import MACPDFCompressor
+
+struct MACPDFCompressorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
